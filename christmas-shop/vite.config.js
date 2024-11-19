fcd45c1dc82a8f214ef5d1bf42a0758971c8ef5d
privatePython <gitarist.pasha@gmail.com>
2024-11-19 21:35:20 +0300

    1

--- christmas-shop/vite.config.js
+++ christmas-shop/vite.config.js
@@ -3,18 +3,24 @@ import  path from 'path';
 
 export default defineConfig(() => {
     return {
-        base: "/test-rs-1-tsak/christmas-shop/",
-        publicDir: "public",
+        base: "/test-rs-1-tsak/christmas-shop",
+        resolve: {
+            alias: {
+                // '@': path.resolve(__dirname, './src'),
+                '@assets': path.resolve(__dirname, './src/assets')
+            }
+        },
         build: {
             sourcemap: true, // enable production source maps
+            rollupOptions: {
+                input: {
+                    main: './index.html',
+                    gifts: './gifts-page.html'
+                }
+            }
         },
         css: {
             devSourcemap: true // enable CSS source maps during development
         },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src')
-            }
-        }
     }
 })
